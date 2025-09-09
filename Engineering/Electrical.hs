bf54f0066ba29efbb403891a532120488a1e2c07
YasminMSoares <158000307+YasminMSoares@users.noreply.github.com>
2025-09-09 20:42:20 -0300

    Update Electrical.hs

--- Engineering/Electrical.hs
+++ Engineering/Electrical.hs
@@ -4,3 +4,11 @@ import Types
 
 tensaoOhm :: Corrente-> Resistencia-> Tensao
 tensaoOhm i r = i * r
+
+potenciaEletricaRI :: Resistencia-> Corrente-> PotenciaEletrica
+potenciaEletricaRI r i = r * i^2
+
+potenciaEletricaVR :: Tensao-> Resistencia-> PotenciaEletrica
+potenciaEletricaVR v r = v^2 / r
+
+
