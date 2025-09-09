1d94f834bcaaa5d2c0bbe0dbcb3e20b940d8620f
YasminMSoares <158000307+YasminMSoares@users.noreply.github.com>
2025-09-09 20:29:25 -0300

    Update Algorithms.hs

--- Algorithms.hs
+++ Algorithms.hs
@@ -7,3 +7,38 @@ quickSort (x:xs) =
         maiores = [a | a <- xs, a >  x]
     in  quickSort menores ++ [x] ++ quickSort maiores
     
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  = merge (mergeSort left) (mergeSort right)
+  where
+    mid = length xs `div` 2
+    (left, right) = splitAt mid xs
+
+    merge [] ys = ys
+    merge xs [] = xs
+    merge (x:xs) (y:ys)
+      | x <= y    = x : merge xs (y:ys)
+      | otherwise = y : merge (x:xs) ys
+      
+insertionSort :: (Ord a) => [a] -> [a]
+insertionSort []     = []
+insertionSort (x:xs) = insert x (insertionSort xs)
+  where
+    insert :: (Ord a) => a -> [a] -> [a]
+    insert y [] = [y]
+    insert y (z:zs)
+      | y <= z    = y : z : zs
+      | otherwise = z : insert y zs
+
+buscarProjeto :: Int -> [Projeto] -> Maybe Projeto
+buscarProjeto _ [] = Nothing
+buscarProjeto idBuscado (p:ps)
+  | idProjeto p == idBuscado = Just p
+  | otherwise                 = buscarProjeto idBuscado ps
+      
+inserirOrdenado :: (Ord a) => a -> [a] -> [a]
+inserirOrdenado x [] = [x] 
+inserirOrdenado x (y:ys)
+  | x <= y    = x : y : ys
+  | otherwise = y : inserirOrdenado x ys 
