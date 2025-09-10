01c51ec96529bc668d7b854f7b1b207de8acffdd
leonardo-p-s <leonardops@gmail.com>
2025-09-09 23:16:42 -0300

    Update Mechanical.hs

--- Engineering/Mechanical.hs
+++ Engineering/Mechanical.hs
@@ -2,6 +2,21 @@ module Engineering.Mechanical where
 
 import Types
 
+-- Engenharia Mecânica
+calcularTorque :: Forca -> Distancia -> Angulo -> Torque
+calcularTorque f r theta = f * r * sin theta
+
+velocidadeAngular :: Velocidade -> Raio -> VelocidadeAngular
+velocidadeAngular v r
+  | r == 0    = 0          -- divisão por zero
+  | otherwise = v / r
+
+aceleracaocentripeta :: Velocidade -> Raio -> Aceleracao
+aceleracaocentripeta v r
+  | r == 0    = 0           -- evita divisão por zero
+  | otherwise = v^2 / r
+
+
 energiaCinetica :: Massa-> Velocidade-> Energia
 energiaCinetica m v = 0.5 * m * v^2
 
