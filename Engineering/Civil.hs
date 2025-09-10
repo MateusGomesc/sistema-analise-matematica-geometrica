0c61a5fd84ce2fef14985c5ae8dce2d1cd82f50f
leonardo-p-s <leonardops@gmail.com>
2025-09-09 23:14:39 -0300

    Update Civil.hs

---
+++ Engineering/Civil.hs
@@ -0,0 +1,9 @@
+module Engineering.Civil where
+
+import Types
+
+volumeConcreto :: Figura -> Volume
+volumeConcreto (Paralelepipedo c l a) = c * l * a
+volumeConcreto (Cilindro r h)         = pi * r^2 * h
+volumeConcreto (Esfera r)             = (4/3) * pi * r^3
+volumeConcreto _                      = 0  -- Outros casos não aplicáveis
