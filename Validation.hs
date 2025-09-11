1f8ec72a6539e867b3e300398102eb69a6f96ada
leonardo-p-s <leonardops@gmail.com>
2025-09-11 08:28:10 -0300

    Update Validation.hs

--- Validation.hs
+++ Validation.hs
@@ -12,21 +12,3 @@ valida opção min max
         putStr "Escolha uma opção: "
         opção <- readLn
         valida opção min max
-
-contarPorTipo :: [Projeto] -> [(TipoProjeto, Int)]
-contarPorTipo projetos =
-  filter (\(_, n) -> n > 0)  -- Filtra n > 0. So lista se tiver pelo menos um projeto daquele tipo
-    [ (Civil,      length (filter (\p -> tipoProjeto p == Civil) projetos))
-    , (Mecanica,   length (filter (\p -> tipoProjeto p == Mecanica) projetos))
-    , (Eletrica,   length (filter (\p -> tipoProjeto p == Eletrica) projetos))
-    , (Estrutural, length (filter (\p -> tipoProjeto p == Estrutural) projetos))
-    ]
-
-projetosEmAtraso :: [Projeto] -> Day -> [Projeto]
-projetosEmAtraso projetos dataAtual =
-  filter emAtraso projetos
-  where
-    emAtraso projeto =
-      case dataFim projeto of
-        Just fim -> fim < dataAtual && status projeto /= Concluido
-        Nothing  -> False
